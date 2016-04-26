64b36c3b8e7fb5de8bb6f48c2954234890192394
Uemit Seren <uemit.seren@gmail.com>
2016-04-26 17:06:51 +0200

    Fixed OpenBLAS installation

--- openblas_install.sh
+++ openblas_install.sh
@@ -4,6 +4,6 @@ cd /tmp/build
 git clone -q --branch=master git://github.com/xianyi/OpenBLAS.git
 (cd OpenBLAS \
     && make DYNAMIC_ARCH=1 NO_AFFINITY=1 NUM_THREADS=32 \
-    && PREFIX=/usr/local make install)
+    && PREFIX=/usr/local make install DYNAMIC_ARCH=1 NO_AFFINITY=1 NUM_THREADS=32)
 cd /
 rm -rf /tmp/build
